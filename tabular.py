370dda330e336c273cc1121325364b3fe3ea5f50
hibozzy18 <hknguruwe@gmail.com>
2019-01-22 08:34:47 +0100

    change range from -5.0:5.0 to -3.0:30

--- tabular.py
+++ tabular.py
@@ -5,7 +5,7 @@ import matplotlib
 matplotlib.use("TKAgg")
 from matplotlib import pyplot as plt
 
-xval = [x for x in pl.frange(-5.0, 5.0, 0.1)]
+xval = [x for x in pl.frange(-3.0, 3.0, 0.1)]
 if(len(sys.argv) == 2):
     option = sys.argv[1]
     if(int(option) == 1):
