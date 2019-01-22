7ad86cc075202a113ea2cdb2f0d9a98787bf06ca
hibozzy18 <hknguruwe@gmail.com>
2019-01-22 08:10:05 +0100

    fill lists

--- tabular.py
+++ tabular.py
@@ -1,4 +1,8 @@
 import sys
+import pylab as pl
 
+xval = [x for x in pl.frange(-5.0, 5.0, 0.1)]
 if(len(sys.argv) == 2):
     option = sys.argv[1]
+    if(int(option) == 1):
+        yval = [x for x in xval]
