e97f15764000498a841716ce1da6b8a59bb3860c
hibozzy18 <hknguruwe@gmail.com>
2019-01-22 08:21:50 +0100

    add usage text for help and list of options

--- tabular.py
+++ tabular.py
@@ -10,6 +10,16 @@ if(len(sys.argv) == 2):
     option = sys.argv[1]
     if(int(option) == 1):
         yval = [x for x in xval]
-        
+
+else:
+    print(""""
+    Wrong argument list!!. 
+    Usage: Tabulation.py N  where is option number.\n
+    N has the following options:
+    1 ---   f(x)\t 
+
+    """)
+    sys.exit(1)
+
 plt.plot(xval, yval)
 plt.show()
